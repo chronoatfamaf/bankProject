85712d4b32f8413127ed74f38d04a1af23d33cd5
Leonardo <leonardo.massimino@globant.com>
2019-01-13 23:21:06 -0300

    Adding Initial Project Models

---
+++ src/main/java/com/bankproject/demo/BankProjectApplication.java
@@ -0,0 +1,14 @@
+package com.bankproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankProjectApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/bankproject/demo/models/RecipientList.java
@@ -0,0 +1,4 @@
+package com.bankproject.demo.models;
+
+public class RecipientList {
+}
